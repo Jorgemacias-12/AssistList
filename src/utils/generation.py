246a71d9fb527b169a89b5b0b56ab33bf6c4d5c5
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2023-05-15 19:16:10 -0600

    Add: generar fecha de cumpleaños y nombres

--- src/utils/generation.py
+++ src/utils/generation.py
@@ -1,17 +1,81 @@
 import random
 import string
-
+from datetime import date, timedelta
 
 def generate_full_name():
 
-    names = ['Juan', 'Pedro', 'María', 'Laura', 'Carlos', 'Ana', 'Luis']
-    last_names = ['García', 'López', 'Martínez', 'Rodríguez', 'Fernández']
+    names = [
+        "Juan",
+        "María",
+        "Carlos",
+        "Ana",
+        "Luis",
+        "Gabriela",
+        "José",
+        "Laura",
+        "Miguel",
+        "Sofía",
+        "Alejandro",
+        "Valentina",
+        "Ricardo",
+        "Carolina",
+        "Eduardo",
+        "Andrea",
+        "Javier",
+        "Natalia",
+        "Pedro",
+        "Isabella"
+    ]
 
-    name = random.choice(names)
-    last_name = random.choice(last_names)
 
-    return f"{name} {last_name}"
 
+    first_lastnames = [
+        "García",
+        "Rodríguez",
+        "González",
+        "Hernández",
+        "López",
+        "Martínez",
+        "Pérez",
+        "Fernández",
+        "Gómez",
+        "Díaz",
+        "Torres",
+        "Ramírez",
+        "Flores",
+        "Silva",
+        "Vargas",
+        "Cruz",
+        "Morales",
+        "Ortega",
+        "Rojas",
+        "Sánchez"
+    ]
+
+    second_lastnames = [
+        "Alvarez",
+        "Castro",
+        "Chavez",
+        "Espinosa",
+        "Guerra",
+        "Lara",
+        "Mendoza",
+        "Miranda",
+        "Montoya",
+        "Navarro",
+        "Orozco",
+        "Ponce",
+        "Quintero",
+        "Ramos",
+        "Reyes",
+        "Salazar",
+        "Soto",
+        "Valdez",
+        "Vega",
+        "Zamora"
+    ]
+
+    return f"{random.choice(names)} {random.choice(first_lastnames)} {random.choice(second_lastnames)}"
 
 def generate_password():
     
@@ -20,3 +84,17 @@ def generate_password():
     password = ''.join(random.choice(characters) for _ in range(12))
 
     return password
+
+def generate_birthdate():
+
+    today = date.today()
+    eighteen_years_ago = today - timedelta(days=365 * 18)
+    twenty_five_years_ago = today - timedelta(days=365 * 25)
+
+    start_date = twenty_five_years_ago.replace(year=twenty_five_years_ago.year + 1)
+    end_date = eighteen_years_ago.replace(year=eighteen_years_ago.year - 1)
+    
+
+    birthdate = start_date + timedelta(days=random.randint(0, (end_date - start_date).days))
+
+    return birthdate.strftime("%d/%m/%Y")
