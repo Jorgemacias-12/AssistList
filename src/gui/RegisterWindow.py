76db52bff144c08cc74416d4a08a76d20f0565f5
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2023-05-17 13:40:10 -0600

    Add: interfaz y código de formulario de registro

--- src/gui/RegisterWindow.py
+++ src/gui/RegisterWindow.py
@@ -1,8 +1,64 @@
 from src.utils.logging import BaseWindow
+from src.utils.css import getCSS
 
+from PyQt5.QtWidgets import QLabel, QLineEdit
+from src.utils.MessageBox import message_box
 
 class RegisterWindow(BaseWindow):
 
     def __init__(self):
 
         super().__init__("src/gui/RegisterWindow.ui")
+
+        self.btn_register.clicked.connect(self.register) 
+
+        # Iterar en los controles para aplicar estilos en común
+        # for component in self.frm_container:
+        for i in range(self.frm_container.count()):
+
+            component = self.frm_container.itemAt(i)
+            widget = component.widget()
+
+            if isinstance(widget, QLabel):
+
+                widget.setStyleSheet(getCSS("src/styles/form-label.css"))
+
+            if isinstance(widget, QLineEdit):
+
+                widget.setStyleSheet(getCSS("src/styles/input.css"))
+
+    def register(self):
+
+        # Obtener información
+        code = self.txt_student_code.text()
+        full_name = self.txt_fullname.text()
+        birthdate = self.de_birthdate.date().toString("dd/MM/yyyy")
+        password = self.txt_password.text()
+
+        # Validaciones aquí
+        if not code:
+            message_box(
+        "¡Atención!", "Por favor, ingresa el código de estudiante", "info")
+            return
+
+        if not len(code) == 9:
+            message_box(
+                "¡Atención!", "código de estudiante no válido", "warning")
+            return
+
+        if not full_name:
+            message_box("¡Atención!", "Por favor, ingresa el nombre completo", "info")
+            return
+
+        if not birthdate:
+            message_box(
+            "¡Atención!", "Por favor, selecciona una fecha de nacimiento", "info")
+            return
+
+        if not password:
+            message_box("¡Atención!", "Por favor, ingresa la contraseña", "info")
+            return
+
+
+        
+        # print(f"{code} - {full_name} -  {birthdate} - {password}")
