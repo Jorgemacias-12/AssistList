540c71db87d9598ce68cf54e91bb09e9af3a25f3
Jorge Antonio Macias Zambrano <jmacias3118@jal.conalep.edu.mx>
2023-05-18 12:47:02 -0600

    Add: la información es añadida correctamente

--- src/gui/AssistanceViewer.py
+++ src/gui/AssistanceViewer.py
@@ -1,4 +1,4 @@
-
+import traceback
 from src.utils.logging import BaseWindow
 
 import os
@@ -7,8 +7,11 @@ import calendar
 
 from src.utils.data import subjects
 from PyQt5.QtWidgets import QComboBox, QTableWidgetItem
+from PyQt5.QtGui import QColor
+from PyQt5.QtCore import QTimer
 from src.utils.css import getCSS
 from src.utils.logging import log
+from datetime import date
 
 FILENAME = "assistances.json"
 DIRECTORY = os.path.join(os.getenv('APPDATA'), 'com.jamr.assistList')
@@ -23,6 +26,10 @@ class AssistanceViewer(BaseWindow):
 
     COLUMNS = 3
 
+    DAY_COLUMN = 0
+    STUDENT_CODE_COLUMN = 1
+    STUDENT_FULLNAME_COLUMN = 2
+
     month = None
     subject = None
 
@@ -30,13 +37,13 @@ class AssistanceViewer(BaseWindow):
 
         super().__init__("src/gui/AssistanceViewer.ui")
 
+        #  use actual month
+        self.month = date.today().strftime("%B")
+
         self.init_components()
 
         self.read_assistance()
-
-        # self.insert_data()
-        self.update_subject()
-        self.update_month()
+        
         self.update_assistances()
 
     def init_components(self):
@@ -65,6 +72,7 @@ class AssistanceViewer(BaseWindow):
 
         self.cmb_subject.currentIndexChanged.connect(self.update_subject)
         self.cmb_month.currentIndexChanged.connect(self.update_month)
+        self.btn_close.clicked.connect(self.handle_error_close)
 
         self.lbl_error.hide()
 
@@ -74,35 +82,82 @@ class AssistanceViewer(BaseWindow):
 
             month_data = self.json_data[self.subject][self.month]
 
-            for column in range(self.COLUMNS):
+            month_data_size = len(month_data)
+            day_data_size = len(month_data.items())
 
-                print(column)
+            total_data_size = month_data_size * day_data_size
 
-                for row in range(len(month_data.keys())):
+            # Set the row and column count
+            self.tbl_assistances.setRowCount(total_data_size)
+            # self.tbl_assistances.setColumnCount(self.COLUMNS)
 
-                    item = QTableWidgetItem("Joder")
+            print(month_data_size, day_data_size)
 
-                    self.tbl_assistances.setItem(row, column, item)
-                # for row, day in month_data.items():
+            row = 0
+
+            for index, (key, data) in enumerate(month_data.items()):
+                
+                for student in data:
                     
-                    # print(row, day)
+                    row += 1
+                    
+                    for column in range(self.COLUMNS):
+                        
+                        if column == self.DAY_COLUMN:
 
-                    # item = QTableWidgetItem(day)
+                            self.tbl_assistances.setItem(row, column, self.create_item(key))
 
-                    # self.lbl_assistances.setItem(int(row), column, item)
+                        if column == self.STUDENT_CODE_COLUMN:
 
+                            self.tbl_assistances.setItem(row, column, self.create_item(student["codigo de estudiante"]))
+
+                        if column == self.STUDENT_FULLNAME_COLUMN:
+
+                            self.tbl_assistances.setItem(row, column, self.create_item(student["nombre"]))
+
+
+                        # print(f"{key} - {student}")
+
+                #         self.tbl_assistances.setItem(
+                #                     row, column, self.create_item(student["nombre"]))
+                # # for column in range(self.COLUMNS):
+                    
+                #     for student in data:
+
+                #         print(student["nombre"])
+
+                    pass
+
+
+            # for row, (key, data) in enumerate(month_data.items()):
+
+                # for column in range(self.COLUMNS):
+
+                    # print(column)
+
+                # for item in data ()
+
+            # for column in range(self.COLUMNS):
+
+            #     for row, (key, data) in enumerate(month_data.items()):
+                    
+            #         for item in data:
+
+            #             print (item)
+                    
         except Exception as ex:
 
-            log(self.logger, f"No hay información disponible {ex}")
+            log(self.logger, f"No hay información disponible {type(ex).__name__}")
+
+            print(type(ex).__name__)
 
             self.lbl_error.setText(
                 f"No hay registro de asistencias en el mes:  {ex}")
 
+            traceback.print_exc()
+
             self.lbl_error.show()
-
-        # for i in range(len(self.json_data[self.subject][self.month]))
-
-        pass
+            self.btn_close.show()
 
     def read_assistance(self):
 
@@ -120,10 +175,15 @@ class AssistanceViewer(BaseWindow):
         self.month = self.cmb_month.currentText()[0:3]
         self.update_assistances()
 
-    # def insert_data(self):
+    def create_item(self, text):
 
-    #     for row, item in enumerate(self.json_data):
+        component = QTableWidgetItem(text)
+        component.setForeground(QColor("black"))
+        component.setBackground(QColor("white"))
 
-    #         print(f"{row} - {item}")
+        return component
+    
+    def handle_error_close(self):
 
-    #     pass
+        self.lbl_error.hide()
+        self.btn_close.hide()
